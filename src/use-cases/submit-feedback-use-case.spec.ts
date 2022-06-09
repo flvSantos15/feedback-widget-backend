d94d9255186670ac72d381113ac1e2db0bbb6767
flvSantos15 <flvSantos300@gmail.com>
2022-06-09 13:29:01 -0300

    finished test

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,16 +1,46 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
 
+const createFeedbackSpy = jest.fn()
+const sendMailSpy = jest.fn()
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
+)
+
 describe('Submit feedback', () => {
   it('should be able to submit a feedback', async () => {
-    const submitFeedback = new SubmitFeedbackUseCase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    )
-
     await expect(submitFeedback.execute({
       type: 'BUG',
       comment: 'example comment',
-      screenshot: 'test.jpg'
+      screenshot: 'data:image/png;base64,9as8fa8f7asas09fas09fs'
     })).resolves.not.toThrow()
+
+    expect(createFeedbackSpy).toHaveBeenCalled()
+    expect(sendMailSpy).toHaveBeenCalled()
+  })
+
+  it('should not be able to submit feedback without type', async () => {
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64,9as8fa8f7asas09fas09fs'
+    })).rejects.toThrow()
+  })
+
+  it('should not be able to submit feedback without comment', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      screenshot: 'data:image/png;base64,9as8fa8f7asas09fas09fs'
+    })).rejects.toThrow()
+  })
+
+  it('should not be able to submit feedback with an invalid screenshots', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'Algum texto',
+      screenshot: 'test.jpg'
+    })).rejects.toThrow()
   })
 })
